e02195b49d0f6ef9dc843ea48312b00ee59c9378
Adrian <adrian.gorniak97@op.pl>
2023-08-07 10:46:17 +0200

    animation of the splashScreen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { GlobalStyle } from "./styles/globalStyles";
 import { Wrapper } from "./styles/Wrapper";
 import { StyledOptions } from "styled-components";
+import { useState } from "react";
 import {
   StyledSplashScreen,
   StyledCircle,
@@ -8,21 +9,74 @@ import {
   StyledHeadline,
   StyledSmallCircle,
 } from "./styles/SplashScreen.styled";
+
 function App() {
+  const [animate, setAnimate] = useState(false);
+
+  const handleAnimate = () => {
+    setAnimate(true);
+
+    setTimeout(() => {
+      setAnimate(false);
+    }, 3000);
+  };
+
   return (
     <Wrapper>
       <GlobalStyle />
-      <StyledSplashScreen>
-        <StyledMiddleCircle>
-          <StyledCircle style={{ right: "75%" }}></StyledCircle>
-          <StyledCircle style={{ right: "30%" }}></StyledCircle>
-          <StyledSmallCircle style={{ bottom: "58%" }} />
-          <StyledHeadline>
+      <StyledSplashScreen onClick={() => handleAnimate()} animate={animate}>
+        <StyledMiddleCircle animate={animate}>
+          <StyledCircle
+            style={{ right: "75%" }}
+            animationprops={{
+              startValue: "translateX(0)",
+              endValue: "translateX(-50%)",
+            }}
+            animate={animate}
+          ></StyledCircle>
+          <StyledCircle
+            style={{ right: "30%" }}
+            animationprops={{
+              startValue: "translateX(0)",
+              endValue: "translateX(-80%)",
+            }}
+            animate={animate}
+          ></StyledCircle>
+          <StyledSmallCircle
+            style={{ bottom: "58%" }}
+            animationprops={{
+              startValue: "translateY(0)",
+              endValue: "translateY(-80%)",
+            }}
+            animate={animate}
+          />
+          <StyledHeadline animate={animate}>
             <h1>Panini Creator</h1>
           </StyledHeadline>
-          <StyledSmallCircle style={{ top: "58%" }} />
-          <StyledCircle style={{ left: "30%" }}></StyledCircle>
-          <StyledCircle style={{ left: "75%" }}></StyledCircle>
+          <StyledSmallCircle
+            style={{ top: "58%" }}
+            animationprops={{
+              startValue: "translateY(0)",
+              endValue: "translateY(80%)",
+            }}
+            animate={animate}
+          />
+          <StyledCircle
+            style={{ left: "30%" }}
+            animationprops={{
+              startValue: "translateX(0)",
+              endValue: "translateX(80%)",
+            }}
+            animate={animate}
+          ></StyledCircle>
+          <StyledCircle
+            style={{ left: "75%" }}
+            animationprops={{
+              startValue: "translateX(0)",
+              endValue: "translateX(80%)",
+            }}
+            animate={animate}
+          ></StyledCircle>
         </StyledMiddleCircle>
       </StyledSplashScreen>
     </Wrapper>
--- src/styles/SplashScreen.styled.tsx
+++ src/styles/SplashScreen.styled.tsx
@@ -1,6 +1,70 @@
-import { styled } from "styled-components";
+import { styled, keyframes } from "styled-components";
 
-export const StyledSplashScreen = styled.div`
+type AnimationPropsType = {
+  startValue?: string;
+  endValue?: string;
+};
+
+type SyledCircleTypes = {
+  animationprops?: AnimationPropsType;
+  animate?: boolean;
+};
+
+type AnimatePropType = {
+  animate: boolean;
+};
+
+const animateOpacitySplashScreen = () => keyframes`
+    0% {
+        opacity:  1;
+    }
+    100% {
+        opacity: 0;
+        display: none;
+    }
+`;
+
+const generateAnimation = (animationProps) => keyframes`
+    from {
+        transform: ${animationProps.startValue};
+    }
+    to {
+        transform: ${animationProps.endValue};
+    }
+`;
+
+const animateMiddleCircle = () => keyframes`
+    
+    0% {
+        width: 1091px;
+        position: absolute;
+    }
+    100% {
+        width: 200%;
+        position: absolute;
+    }
+
+`;
+
+const animateHeadlineBefore = () => keyframes`
+    from {
+        left: 0;
+    }
+    to {
+        left: 100%;
+    }
+`;
+const animateHeadlineAfter = () => keyframes`
+    from {
+        right: 0;
+    }
+    to {
+        right: 100%;
+       
+    }
+`;
+
+export const StyledSplashScreen = styled.div<AnimatePropType>`
   width: 100%;
   height: 100%;
   z-index: 1;
@@ -11,17 +75,22 @@ export const StyledSplashScreen = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  animation: ${({ animate }) => (animate ? animateOpacitySplashScreen : "none")}
+    3s linear;
 `;
 
-export const StyledCircle = styled.div`
+export const StyledCircle = styled.div<SyledCircleTypes>`
   width: 1091px;
   height: 1091px;
   border-radius: 100%;
   border: 1px solid black;
   position: absolute;
+  animation: ${({ animationprops, animate }) =>
+      animate ? generateAnimation(animationprops) : "none"}
+    3s linear;
 `;
 
-export const StyledMiddleCircle = styled.div`
+export const StyledMiddleCircle = styled.div<AnimatePropType>`
   min-width: 1091px;
   height: 1091px;
   border-radius: 100%;
@@ -31,23 +100,51 @@ export const StyledMiddleCircle = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  animation: ${({ animate }) => (animate ? animateMiddleCircle : "none")} 3s
+    linear;
 `;
 
-export const StyledSmallCircle = styled.div`
+export const StyledSmallCircle = styled.div<SyledCircleTypes>`
   height: 632px;
   width: 632px;
   border-radius: 100%;
   border: 1px solid black;
   position: absolute;
+  animation: ${({ animationprops, animate }) =>
+      animate ? generateAnimation(animationprops) : "none"}
+    3s linear;
 `;
 
-export const StyledHeadline = styled.div`
+export const StyledHeadline = styled.div<AnimatePropType>`
   width: 98%;
   padding: 10vh 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border-top: 1px solid black;
-  border-bottom: 1px solid black;
+  position: relative;
+  ::before {
+    content: "";
+    width: 100%;
+    max-width: 1091px;
+    height: 1px;
+    background-color: black;
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: ${({ animate }) => (animate ? animateHeadlineBefore : "none")} 3s
+      linear;
+  }
+  ::after {
+    content: "";
+    width: 100%;
+    max-width: 1091px;
+    height: 1px;
+    background-color: black;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    animation: ${({ animate }) => (animate ? animateHeadlineAfter : "none")} 3s
+      linear;
+  }
 `;
