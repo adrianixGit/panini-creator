61fa64f25a2b668182cd63a384b5bca5ed954a95
Adrian <adrian.gorniak97@op.pl>
2023-08-10 09:28:39 +0200

    deleted after and befor from global styles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 
-*, ::after, ::before {
+* {
         margin:0;
         padding: 0;
         box-sizing: border-box;
